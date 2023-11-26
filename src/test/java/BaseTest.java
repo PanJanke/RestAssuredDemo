aa57ab7c4d9100f46746ced6d988ec24a624d31e
Jan Krol <jan.krol.legowski@gmail.com>
2023-11-26 20:56:06 +0100

    BaseTest- request,response configuration

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,33 @@
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+
+    protected RequestSpecification requestSpec;
+    protected ResponseSpecification responseSpec;
+
+    @BeforeClass
+    public void setUp(){
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri("htttp://localhost:3000")
+                .setBasePath("posts")
+                .setContentType(ContentType.JSON)
+                .build();
+
+        responseSpec = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .build();
+
+        RequestLoggingFilter reqLog = new RequestLoggingFilter();
+        ResponseLoggingFilter resLog = new ResponseLoggingFilter();
+        RestAssured.filters(resLog,reqLog);
+    }
+}
