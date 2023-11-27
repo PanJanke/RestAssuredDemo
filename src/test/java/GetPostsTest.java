7457f71d725558d4be44a0692bf5c440c72c6abd
Jan Krol <jan.krol.legowski@gmail.com>
2023-11-27 10:20:49 +0100

    add GetPosts Test

---
+++ src/test/java/GetPostsTest.java
@@ -0,0 +1,25 @@
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GetPostsTest extends BaseTest {
+    @Test
+    public void getPosts(){
+        given()
+                .spec(requestSpec)
+        .when()
+                .get()
+        .then()
+                .spec(responseSpec)
+                .assertThat().body("title[0]", Matchers.equalTo("pierwszy"))
+                .assertThat().body("author[0]", Matchers.equalTo("Bartek"))
+        .and()
+                .assertThat().body("title[1]", Matchers.equalTo("drugi"))
+                .assertThat().body("author[1]", Matchers.equalTo("Tomek"))
+        .and()
+                .assertThat().body("title[2]", Matchers.equalTo("trzeci"))
+                .assertThat().body("author[2]", Matchers.equalTo("Kasia"));
+    }
+
+}
