10d33767e1362b29190d8af2c491a7e5b9030ba8
Jan Krol <jan.krol.legowski@gmail.com>
2023-11-27 17:57:30 +0100

    User model, basic auth tests

--- src/test/java/BasicAuthTest.java
+++ src/test/java/BasicAuthTest.java
@@ -1,3 +1,4 @@
+import model.User;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
@@ -6,13 +7,17 @@ import java.util.concurrent.TimeUnit;
 import static io.restassured.RestAssured.given;
 
 public class BasicAuthTest extends BaseTest {
+
+    private final String authURL = "https://postman-echo.com/basic-auth";
     @Test
     public void basicAuthCorrectCredentialsTest(){
+        User user = new User("postman","password");
 
         given()
-                .auth().basic("postman","password")
+                .auth().basic(user.getUsername(), user.getPassword())
         .when()
-                .get("https://postman-echo.com/basic-auth")
+                .get(authURL)
+
         .then()
                 .spec(responseSpec)
                 .assertThat().body(Matchers.containsStringIgnoringCase("true"));
@@ -20,22 +25,24 @@ public class BasicAuthTest extends BaseTest {
 
     @Test
     public void basicAuthIncorrectCredentialsTest(){
+        User user = new User("postman","WrongPassw0rd");
 
         given()
-                .auth().basic("postman","WronGpassword")
+                .auth().basic(user.getUsername(), user.getPassword())
         .when()
-                .get("https://postman-echo.com/basic-auth")
+                .get(authURL)
         .then()
                 .assertThat().statusCode(401);
     }
 
     @Test
     public void ResponseTimeTest(){
+        User user = new User("postman","password");
 
         given()
-                .auth().basic("postman","password")
+                .auth().basic(user.getUsername(), user.getPassword())
         .when()
-                .get("https://postman-echo.com/basic-auth")
+                .get(authURL)
         .then()
                 .spec(responseSpec)
                 .time(Matchers.lessThan(3000L), TimeUnit.MILLISECONDS);
